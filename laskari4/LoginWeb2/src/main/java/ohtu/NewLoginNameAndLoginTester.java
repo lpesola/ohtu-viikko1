69d33b3fa53f42f50726abbe92aed1c931a78cb3
lpesola <laura.pesola@helsinki.fi>
2012-04-12 19:04:11 +0300

    tehtävä 2 loppuun

--- laskari4/LoginWeb2/src/main/java/ohtu/NewLoginNameAndLoginTester.java
+++ laskari4/LoginWeb2/src/main/java/ohtu/NewLoginNameAndLoginTester.java
@@ -11,25 +11,56 @@ public class NewLoginNameAndLoginTester {
         
         WebDriver driver = new HtmlUnitDriver();
 
+        // etusivu
         driver.get("http://localhost:8080");
         System.out.println(driver.getPageSource());
+        
+        // uusi tunnus
         WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
 
         System.out.println("==");
 
+        // uuden tunnuksen submittointi
         System.out.println(driver.getPageSource());
         element = driver.findElement(By.name("username"));
         element.sendKeys("newuser");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("newp4assword");
+        element.sendKeys("newp4ssword");
         element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys("newp4assword");
+        element.sendKeys("newp4ssword");
         element = driver.findElement(By.name("add"));
         element.submit();
 
         System.out.println("==");
         System.out.println(driver.getPageSource());
+       
+         // takaisin etusivulle
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+       
+        // logataan ulos
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+       
+        // mennään login-sivulle
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        System.out.println("==");
+
+        // ja logataan taas sisään
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("newuser");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("newp4ssword");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
 
     }
 }
