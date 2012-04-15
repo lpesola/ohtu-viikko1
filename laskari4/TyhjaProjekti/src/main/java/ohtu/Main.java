a0e9dd524011a99506644745fa7b564fbf52a27e
lpesola <laura.pesola@helsinki.fi>
2012-04-15 22:18:01 +0300

    tehtävät 5 & 6

--- laskari4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ laskari4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,14 +1,18 @@
+package ohtu;
+
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStream;
+import ohtu.Palautukset;
+import ohtu.Palautus;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.io.IOUtils;
- 
+
 public class Main {
  
     public static void main(String[] args) throws IOException {
-        String studentNr = "12345678";
+        String studentNr = "13470892";
         if ( args.length>0) {
             studentNr = args[0];
         }
@@ -29,10 +33,26 @@ public class Main {
         Gson mapper = new Gson();
         Palautukset palautukset = mapper.fromJson(bodyText, Palautukset.class);
  
-        System.out.println("oliot:");
-        for (Palautus palautus : palautukset.getPalautukset()) {
-            System.out.println( palautus );
+        Palautus palautus = palautukset.getPalautukset().get(0);
+        int lastIndex = palautukset.getPalautukset().size()-1;
+
+        System.out.println("nimi: " + palautus.getEtunimi()+ " "+ palautus.getSukunimi()
+                + " opiskelijanumero: " + palautus.getOpiskelijanumero() + " \n"
+                + "miniprojekti: "+palautukset.getPalautukset().get(lastIndex).getTehtavat());
+        
+        int tehtäviäYhteensä = 0;
+        int tuntejaYhteensä = 0;
+        for (int i=0; i < lastIndex; i++) {
+            Palautus p = palautukset.getPalautukset().get(i);
+            System.out.println("Viikko: " + p.getViikko() + ", tehtävät: " 
+                    + p.getTehtavat() + ", yhteensä "+p.getTehtavia()
+                    +" Aikaa kului "+ p.getTunteja()+" tuntia");
+            tehtäviäYhteensä = tehtäviäYhteensä + p.getTehtavia();
+            tuntejaYhteensä = tuntejaYhteensä + p.getTunteja();
         }
+        
+        System.out.println("Yhteensä "+tehtäviäYhteensä+ " tehtävää, joihin mennyt "
+                + tuntejaYhteensä + " tuntia");
  
     }
 }
